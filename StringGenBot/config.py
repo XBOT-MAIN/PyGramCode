debd3e2e3a89cf583189aff61c2d23d553dab24e
TEAM X <94202479+XBOT-MAIN@users.noreply.github.com>
2021-12-09 13:07:22 +0200

    Add files via upload

--- StringGenBot/config.py
+++ StringGenBot/config.py
@@ -1,5 +1,9 @@
-class Config:
-    API_ID: int = "1714588"
-    API_HASH: str = "78c27bf90c81f15a8af4aa0aeeadfc42"
-    BOT_TOKEN: str = "2129407300:AAG7By5cVad9jq4LmxcTYetSuUwoT4NjfHo"
-    LOG_CHANNEL: int = "-1001521136796"
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+
+    API_HASH = os.environ.get("API_HASH", "")
